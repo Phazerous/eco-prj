17af3ee80e0bcedd8fd862d86c234edefac9538b
Elias Ainsworth <worldlolxd121@gmail.com>
2023-02-20 21:11:21 +0300

    feat: instal swagger and setting configuration swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Eco project')
+    .setDescription('card transaction tracking project')
+    .setVersion('1.0')
+    .addTag('eco')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
